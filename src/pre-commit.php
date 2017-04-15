6e6b62bb9562a1021edc99ad99daa9ba8c04f1ee
Evgeny Glebov <eglebov@4xxi.com>
2017-04-15 16:30:08 +0300

    Fixed pre-commit hook notation

--- src/pre-commit.php
+++ src/pre-commit.php
@@ -3,7 +3,7 @@
 
 require __DIR__ . '/../../vendor/autoload.php';
 
-use QC\Tool\CodeQualityTool\CodeQualityTool;
+use QC\Tool\CodeQualityTool;
 
 $application = new CodeQualityTool();
 $application->run();
